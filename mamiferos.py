7c01572b8c579f56b00bb669cf7f4eea9d118872
rsancho64 <rsancho@iesrioarba.es>
2024-10-25 14:08:45 +0200

    .

---
+++ mamiferos.py
@@ -0,0 +1,10 @@
+#! /usr/bin/python3
+ 
+from gatos import gato
+
+# if_name__ == "__main__":
+      
+g = gato()
+g.pedirComida()
+g.comer()
+g.pedirComida()
--- perros.py
+++ perros.py
@@ -1,1 +1,64 @@
-/usr/bin/python3
+#! /usr/bin/python3
+
+class perro:
+    
+    __name = None
+    __hambre = True
+    __sleepy = True
+    __fear = False
+    __happy = True
+    
+    def __init__(self, aname = "firulais") -> None:
+        self.__name = aname
+        self.ladra()
+
+    def ladra(self):
+        print(f"{self.__name}: WOF")
+
+    def pegar(self):
+        self.__fear = True
+
+    def ataca(self):
+        if self.__fear:
+            print(f"{self.__name}: mieditooo")
+        else:
+            print(f"{self.__name}: GRRRRRR")
+
+    def traePelota(self, color="rojo"):
+        return "pelota: " + color
+
+    def comer(self):
+        self.__hambre = False
+
+    def dormir(self):
+        self.__sleepy = False
+
+    def pedirComida(self):
+        if self.__hambre:
+            self.ladra()
+
+    def tocar(self):
+        if self.__fear:
+            self.ladra()
+            self.__fear = False
+        else:
+            self.__happy = True
+            self.pedirComida()
+
+if __name__ == "__main__":
+    
+    p0 = perro("sultan")
+
+    meTrae = p0.traePelota()
+    print(meTrae)
+    
+    meTrae = p0.traePelota("verde")
+    print(meTrae)
+
+    p0.dormir()
+    p0.pedirComida()
+    p0.comer()
+    p0.ataca()   
+    p0.dormir()
+    
+    
